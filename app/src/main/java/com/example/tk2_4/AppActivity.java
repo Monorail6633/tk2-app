2d864d6ce140d452ed5a65c8eaa47def7cce9813
Monorail6633 <128348383+Monorail6633@users.noreply.github.com>
2023-09-18 01:58:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tk2_4/AppActivity.java
@@ -0,0 +1,13 @@
+package com.example.tk2_4;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+    }
+}
---
+++ app/src/main/java/com/example/tk2_4/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.tk2_4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("LoginActivity", "Login button clicked");
+                Intent intent = new Intent(LoginActivity.this, AppActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
